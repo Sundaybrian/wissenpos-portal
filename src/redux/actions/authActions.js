ae5373ca2220ed0d5d71d65000c0dd47abb8da07
Brian Sunday <brians931@gmail.com>
2020-12-10 19:05:07 +0300

    add actions

---
+++ src/redux/actions/authActions.js
@@ -0,0 +1,31 @@
+import { SET_ERRORS, SET_USER, LOADING_UI, CLEAR_ERRORS } from "../types";
+import axios from "axios";
+
+export const loginUser = (userData, history) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+
+    axios
+        .post("/accounts/login", userData)
+        .then((res) => {
+            const token = `Bearer ${res.data.token}`;
+            localStorage.setItem("token", token);
+            axios.defaults.headers.common["Authorization"] = token;
+
+            dispatch(setUserData(res.data.user));
+            dispatch({ type: CLEAR_ERRORS });
+            history.push("/dashboard");
+        })
+        .catch((err) => {
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data,
+            });
+        });
+};
+
+const setUserData = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
