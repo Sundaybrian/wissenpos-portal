6cc90a251def00649195f6ed84f8b4de599aa7e3
Brian Sunday <brians931@gmail.com>
2020-12-10 19:07:39 +0300

    setting up auth token and setting a default endpoint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import PrivateRoute from "./utils.js/PrivateRoute";
 import jwtDecode from "jwt-decode";
-
+// utils
+import config from "./utils.js/config";
 // MUi stuff
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
@@ -14,22 +15,25 @@ import Dashboard from "./components/pages/Dashboard/Dashboard";
 import Landing from "./components/pages/Landing/Landing";
 import Login from "./components/pages/Login/Login";
 import Register from "./components/pages/Register/Register";
+import axios from "axios";
 
 const theme = createMuiTheme({
     typography: {
         useNextVariants: true,
     },
 });
+// decidin on the backend service to use
+axios.defaults.baseURL = config["BACKEND_SERVICE"];
 
 // checking for jwt
 let authenticated;
 if (localStorage.token) {
     const decodeToken = jwtDecode(localStorage.token);
     if (decodeToken.exp * 1000 < Date.now()) {
-        window.location.href = "/login";
-        authenticated = true;
-    } else {
         authenticated = false;
+        window.location.href = "/login";
+    } else {
+        authenticated = true;
     }
 }
 
