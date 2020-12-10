6424682d9a18a7bec22120f5e11b34b80386b64e
Brian Sunday <brians931@gmail.com>
2020-12-10 19:03:45 +0300

    add root reducer for combining reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+//redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./index.css";
