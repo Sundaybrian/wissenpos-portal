eee31f5854ab97ca616253b39f5186db7517d6a7
Brian Sunday <brians931@gmail.com>
2020-12-10 19:04:17 +0300

    add auth reducer

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,7 +1,31 @@
-const initialState = {};
+import {
+    SET_ERRORS,
+    SET_USER,
+    LOADING_UI,
+    SET_AUTHENTICATED,
+    SET_UAUTHENTICATED,
+} from "../types";
+
+const initialState = {
+    user: null,
+    authenticated: false,
+};
 
 const auth = (state = initialState, action) => {
     switch (action.type) {
+        case SET_AUTHENTICATED:
+            return {
+                ...state,
+                authenticated: true,
+            };
+        case SET_UAUTHENTICATED:
+            return initialState;
+        case SET_USER:
+            return {
+                ...state,
+                authenticated: true,
+                user: action.payload,
+            };
         default:
             return state;
     }
