788b6517b9d1233e86212f5f0e647fa494784939
Brian Sunday <brians931@gmail.com>
2021-02-18 13:12:36 +0300

    remove react router v6

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -16,7 +16,7 @@ export const loginUser = (userData, history) => (dispatch) => {
             history.push("/dashboard");
         })
         .catch((err) => {
-            dispatch({
+            return dispatch({
                 type: SET_ERRORS,
                 payload: err.response.data,
             });
