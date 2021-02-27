6c0fbbc172bdb7a6c42b956bd6373bb8217431f0
Brian Sunday <brians931@gmail.com>
2021-02-27 21:37:40 +0300

    add more types

--- src/redux/types.js
+++ src/redux/types.js
@@ -3,12 +3,20 @@ export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
 export const SET_UAUTHENTICATED = "SET_UAUTHENTICATED";
 export const SET_USER = "SET_USER";
 export const LOADING_USER = "LOADING_USER";
+
 // UI REDUCER TYPES
 export const SET_ERRORS = "SET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const LOADING_UI = "LOADING_UI";
 
+// SHARED TYPES
 export const SET_CURRENT = "SET_CURRENT";
 export const CLEAR_CURRENT = "CLEAR_CURRENT";
-// export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
-// export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
+
+//Staff types
+export const ADD_STAFF = "ADD_STAFF";
+export const DELETE_STAFF = "DELETE_STAFF";
+export const SET_STAFF = "SET_STAFF";
+export const LOAD_STAFF = "LOAD_STAFF";
+export const STAFF_ERROR = "STAFF_ERROR";
+export const EDIT_STAFF = "EDIT_STAFF";
