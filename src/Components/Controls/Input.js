18d8229cab819c25b462a89151213b43285809f4
Brian Sunday <brians931@gmail.com>
2021-03-14 18:36:28 +0300

    reusable textfield

---
+++ src/Components/Controls/Input.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { TextField } from "@material-ui/core";
+
+function Input(props) {
+    const { name, value, onChange, label } = props;
+    return (
+        <TextField
+            variant="outlined"
+            name={name}
+            value={value}
+            onChange={onChange}
+            label={label}
+        />
+    );
+}
+
+export default Input;
