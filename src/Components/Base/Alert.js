ef91be4337ff2450a964ef7dc01d4a9fc06cba6e
Brian Sunday <brians931@gmail.com>
2021-03-27 01:05:49 +0300

    Add: alert component

--- src/Components/Base/Alert.js
+++ src/Components/Base/Alert.js
@@ -1,8 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MuiAlert from "@material-ui/lab/Alert";
+import { Snackbar } from "@material-ui/core";
+import PropTypes from "prop-types";
+import Slide from "@material-ui/core/Slide";
+// redux
+
+import { connect } from "react-redux";
 
 function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
+    const {
+        ui: { errors, severity },
+    } = props;
+
+    const [snackOpen, setSnackOpen] = React.useState({
+        isOpen: false,
+        message: "",
+        type: "success",
+    });
+    /* eslint-disable no-unused-vars */
+    const [state, setState] = React.useState({
+        open: false,
+        Transition: Slide,
+    });
+
+    /* eslint-disable react-hooks/exhaustive-deps */
+    useEffect(() => {
+        setSnackOpen({
+            ...state,
+            isOpen: true,
+            message: errors !== null ? errors.message : "",
+            type: severity || "success",
+        });
+    }, [errors]);
+
+    const snackClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setSnackOpen({
+            ...state,
+            isOpen: false,
+        });
+    };
+
+    return (
+        <Snackbar
+            open={snackOpen.isOpen}
+            autoHideDuration={6000}
+            onClose={snackClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            TransitionComponent={state.Transition}
+        >
+            <MuiAlert onClose={snackClose} severity={snackOpen.type}>
+                {snackOpen.message}
+            </MuiAlert>
+        </Snackbar>
+    );
+    // return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-export default Alert;
+Alert.propTypes = {
+    ui: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    ui: state.ui,
+});
+
+export default connect(mapStateToProps)(Alert);
