e95174dad321e3d30ca0688eaef069f4661af3d0
Brian Sunday <brians931@gmail.com>
2021-03-27 00:15:21 +0300

    Add: intial company form

--- src/Components/Pages/Company/Profile/CompanyForm.js
+++ src/Components/Pages/Company/Profile/CompanyForm.js
@@ -43,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function CompanyForm(props) {
-    const {} = props;
+    const { handleSubmit } = props;
     const classes = useStyles();
 
     const handleSubmit = (values, actions) => {};
