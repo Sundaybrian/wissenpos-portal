f826fa18c43f22785ec06be67fba590be90efb42
Brian Sunday <brians931@gmail.com>
2021-03-29 00:49:51 +0300

    Update: remove junk code and triggering preview panel based on state

--- src/Components/Pages/Company/CompanyMenu/MealList.js
+++ src/Components/Pages/Company/CompanyMenu/MealList.js
@@ -17,26 +17,11 @@ function MealsList(props) {
         company,
         menu: { menu, currentCategory },
     } = props;
-    // const [meals, setMeals] = useState([
-    //     {
-    //         id: 1,
-    //         name: "Boiled meat",
-    //         price: 200,
-    //         description: "Best meat to salivate over",
-    //         category_id: 1,
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "Boiled meat 2",
-    //         price: 200,
-    //         description: "Best meat to salivate over",
-    //         category_id: 1,
-    //     },
-    // ]);
 
     useEffect(() => {
         // fetch meals based on category
         fetchMealsByCategory(company[0].id, menu.id, category.id);
+        setMeal(null);
     }, [category.id]);
 
     return (
@@ -55,7 +40,13 @@ function MealsList(props) {
                         <ListItemText primary={m.name} />
                     </ListItem>
                 ))}
-            <ListItem button onClick={() => setAddMeal(category)}>
+            <ListItem
+                button
+                onClick={() => {
+                    setAddMeal(category);
+                    setMeal(null);
+                }}
+            >
                 <ListItemText primary="Add Meal+" />
             </ListItem>
         </>
