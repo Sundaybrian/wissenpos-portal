8155fca1fa37c74c52df9f41b6cbb71c61362ab7
Brian Sunday <brians931@gmail.com>
2021-03-28 22:04:48 +0300

    Add: meal for preview of meals

--- src/Components/Pages/Company/CompanyMenu/Meal.js
+++ src/Components/Pages/Company/CompanyMenu/Meal.js
@@ -1,24 +1,68 @@
-import { Paper } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: theme.spacing(3),
     },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
+    media: {
+        height: 240,
+    },
+
+    card: {
+        maxWidth: 945,
     },
 }));
-// will hold the menu name and menu items
-function Meal(props) {
+
+export default function Meal(props) {
+    const classes = useStyles();
     const { meal } = props;
 
-    console.log(meal, "8888888888888888888888888");
-    const classes = useStyles();
-    return <Paper className={classes.paper}>{meal.name}</Paper>;
-}
+    return (
+        <div className={classes.root}>
+            <Typography variant="h5" component="h6" gutterBottom>
+                {meal.name} Preview
+            </Typography>
 
-export default Meal;
+            <Card className={classes.card}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image={meal.image_url}
+                        title={meal.name}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {meal.name}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                        >
+                            {meal.description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        price: ksh {meal.price}
+                    </Button>
+                    <Button size="small" color="primary">
+                        Quantity: {meal.quantity}
+                    </Button>
+                </CardActions>
+            </Card>
+        </div>
+    );
+}
