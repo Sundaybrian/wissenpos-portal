88187c42e295078ab678a70a0d70f50d512f3890
Brian Sunday <brians931@gmail.com>
2021-03-31 21:49:30 +0300

    Add: editMealCategory props down

--- src/Components/Pages/Company/CompanyMenu/AddMealSideBar.js
+++ src/Components/Pages/Company/CompanyMenu/AddMealSideBar.js
@@ -12,7 +12,15 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function AddMealSideBar(props) {
-    const { title, setAddMeal, addMealCategory, menuID, companyID } = props;
+    const {
+        title,
+        setAddMeal,
+        addMealCategory,
+        editMealCategory,
+        menuID,
+        companyID,
+        addOREditMeal,
+    } = props;
     const classes = useStyles();
     const { pathname } = useLocation();
     const [categoryID] = pathname.split("/").slice(-1); // splitting and destructuring last item
@@ -28,12 +36,31 @@ function AddMealSideBar(props) {
         // handleClose();
     };
 
+    const handleMealEdit = (values, action) => {
+        const mealID = addOREditMeal !== null ? addOREditMeal.id : 0;
+        const mealData = {
+            ...values,
+            category_id: parseInt(categoryID),
+        };
+
+        editMealCategory(
+            companyID,
+            menuID,
+            parseInt(categoryID),
+            mealID,
+            mealData
+        );
+        setAddMeal(null);
+    };
+
     return (
         <MenuSidebar title={title}>
             <div className={classes.mealForm}>
                 <MealForm
                     handleMealSubmit={handleMealSubmit}
+                    handleMealEdit={handleMealEdit}
                     setAddMeal={setAddMeal}
+                    addOREditMeal={addOREditMeal}
                 />
             </div>
         </MenuSidebar>
