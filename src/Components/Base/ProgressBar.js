83632f69f6ab75efb9f71b712709fc4902485d90
Brian Sunday <brians931@gmail.com>
2021-04-02 02:55:16 +0300

    Rendering progress bar width depending on progress state

--- src/Components/Base/ProgressBar.js
+++ src/Components/Base/ProgressBar.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../../Hooks/useStorage";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles((theme) => ({
-    progressBar: {},
+    progressBar: {
+        height: "5px",
+        background: theme.palette.primary,
+        marginTop: "20px",
+    },
 }));
 
 function ProgressBar(props) {
     const classes = useStyles();
     const { file, setFile } = props;
     const { url, progress } = useStorage(file);
-    console.log(progress, url);
-    return <div className={classes.progressBar}>{progress}</div>;
+
+    useEffect(() => {
+        if (url) {
+            setFile(null);
+        }
+    }, [url, setFile]);
+
+    return (
+        <div
+            className={classes.progressBar}
+            style={{ width: progress + "%" }}
+        ></div>
+    );
 }
 
 export default ProgressBar;
