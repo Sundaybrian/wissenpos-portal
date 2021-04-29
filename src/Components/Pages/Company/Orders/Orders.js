df28b5a25a74a5f39f586fd80bb006ccde995a4a
Brian Sunday <brians931@gmail.com>
2021-04-29 14:19:58 +0300

    add: condtional render order detail in orders

--- src/Components/Pages/Company/Orders/Orders.js
+++ src/Components/Pages/Company/Orders/Orders.js
@@ -1,13 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Content from "../../../Layout/Content/Content";
 import OrderTable from "./OrderTable";
+import OrderDetail from "./OrderDetail";
 
-function Orders() {
+import { connect } from "react-redux";
+//mui
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({}));
+
+function Orders(props) {
+    const {
+        order: { loading, currentOrder },
+    } = props;
+
     return (
         <Content>
             <OrderTable />
+            {currentOrder && (
+                <OrderDetail currentOrder={currentOrder} loading={loading} />
+            )}
         </Content>
     );
 }
 
-export default Orders;
+const mapStateToProps = (state) => ({
+    order: state.order,
+});
+
+const mapActionsToProps = {};
+
+Orders.propTypes = {
+    order: PropTypes.object.isRequired,
+};
+export default connect(mapStateToProps, mapActionsToProps)(Orders);
