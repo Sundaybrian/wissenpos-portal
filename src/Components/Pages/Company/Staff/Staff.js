66d48c4a206da9718f36786a0a21ceec87aeec5d
Brian Sunday <brians931@gmail.com>
2021-08-08 14:28:39 +0300

    add: linter minor clean up

--- src/Components/Pages/Company/Staff/Staff.js
+++ src/Components/Pages/Company/Staff/Staff.js
@@ -1,37 +1,17 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import StaffSummaryCards from './StaffSummary';
-import makeStyles from '@material-ui/core/styles/makeStyles';
 import Content from '../../../Layout/Content/Content';
 import { connect } from 'react-redux';
-import { addStaff, deleteStaff, fetchStaff } from '../../../../Redux/actions/staffManagementActions';
+import { addStaff, deleteStaff } from '../../../../Redux/actions/staffManagementActions';
 import StaffTable from './StaffTable';
 import StaffProfile from './StaffProfile';
 import { first } from 'lodash';
 import { isEmpty, isLoaded } from 'react-redux-firebase';
 import Loader from '../../../Base/Loader';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-  },
-  paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-
-  grow: {
-    flexGrow: 1,
-  },
-  deleteButton: {
-    marginLeft: theme.spacing(1),
-  },
-}));
-
 function Staff(props) {
-  const classes = useStyles();
-
-  const { company, addStaff, deleteStaff, fetchStaff } = props;
+  const { company, addStaff, deleteStaff } = props;
 
   const [confirmDialog, setConfirmDialog] = useState({
     isOpen: false,
@@ -48,7 +28,7 @@ function Staff(props) {
     console.log(ids);
   };
 
-  // modal for driver data
+  // modal for staff data
   const openInPopup = rider => {
     setRecord(rider);
     setOpenPopup(true);
@@ -100,7 +80,6 @@ const mapStateToProps = state => {
 const mapActionsToProps = {
   addStaff,
   deleteStaff,
-  fetchStaff,
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(Staff);
